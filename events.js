923ae51b8f6b8532ae1da7223ec714bc639c5786
lyz <702368372@qq.com>
2014-09-26 14:11:07 +0800

    first commit

---
+++ events.js
@@ -0,0 +1,43 @@
+var events = {}
+
+window.on = function(name, callback) {
+  var list = events[name] || (events[name] = [])
+  list.push(callback)
+}
+
+window.off = function(name, callback) {
+  if (!(name || callback)) {
+    events = data.events = {}
+    return seajs
+  }
+
+  var list = events[name]
+  if (list) {
+    if (callback) {
+      for (var i = list.length - 1; i >= 0; i--) {
+        if (list[i] === callback) {
+          list.splice(i, 1)
+        }
+      }
+    }
+    else {
+      delete events[name]
+    }
+  }
+
+  return seajs
+}
+
+window.emit = function(name, data) {
+  var list = events[name]
+
+  if (list) {
+    // Copy callback lists to prevent modification
+    list = list.slice()
+
+    // Execute event callbacks, use index because it's the faster.
+    for(var i = 0, len = list.length; i < len; i++) {
+      list[i](data)
+    }
+  }
+}
